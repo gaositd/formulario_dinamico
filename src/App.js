ac0829780c096ef8d08c3025da818b8cb087ace0
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-11-07 17:18:25 -0600

    estructura hecha

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <main></main>
+      <header className="App-header">
+        <h1 className="App-title">Formulario Dinámico</h1>
+      </header>
+      <main className="App-divide">
+        <div className="App-boxSide ">
+          <h1 className="App-title">Elelmentos del Formulario</h1>
+        </div>
+        <div className="App-boxSide">
+          <h1 className="App-title">Agregado Dinámico</h1>
+        </div>
+      </main>
     </div>
   );
 }
